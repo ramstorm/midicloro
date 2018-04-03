75f47be1de21ee0b66d5d32d73ceaf48111e5bca
csantinc <csantinc@gmail.com>
2018-04-03 15:35:08 +0000

    startm.sh autostart script updated

--- startm.sh
+++ startm.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 cd $1
+#Initial start. We wait 15s to start the loop in order not to start different midicloro instances in parallel
+./midicloro &
+sleep 15s
 while true; do
   if ! ps aux | grep -v 'grep' | grep -v 'startm' | grep 'midicloro' ; then
     ./midicloro &
   else
-    exit 0
+    sleep 5s
   fi
-  sleep 5s
 done
